3f6ea4307f265d22b06c97a14fe8022878015279
ZeroKun265 <davidegiambirtone265@gmail.com>
2026-07-17 22:18:40 +0200

    idk

--- demo.gd
+++ demo.gd
@@ -1,7 +1,5 @@
-extends Node
+extends Node2D
 
-@onready var time_left_ms = 0
-@onready var debug_code = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	print($"/root/StateMachine".debug_code)
+	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
