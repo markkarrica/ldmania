48805ff80ea309125a1079db04079b936063f6f5
Mark Karrica <mark.karrica@gmail.com>
2026-07-22 23:58:54 +0200

    LEL

--- minigames/hado_pizza/hado_pizza.gd
+++ minigames/hado_pizza/hado_pizza.gd
@@ -1,18 +1,28 @@
 extends Minigame
 
-var actions: Array[String] = ["mode_down", "move left", "move right", "move_up"]
-var shuffled_actions: Array[String]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	shuffled_actions = Util.deterministic_shuffle(actions)
+	var difficulty: int = StateMachine.difficulty
+	var directions: Array[InputPrompt.Direction] = []
+	for i in range(0, difficulty):
+		var dir = Util.random.randi_range(1, 4)
+		directions.append(dir)
+	$InputPrompt.init(directions)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"): #Debug win condition
-		minigame_end(true, StateMachine.difficulty)
-	if event.is_action_pressed("jump"): # Debug loss
-		minigame_end(false, 0)
+	pass
+	#if event.is_action_pressed("interact"): #Debug win condition
+		#minigame_end(true, StateMachine.difficulty)
+	#if event.is_action_pressed("jump"): # Debug loss
+		#minigame_end(false, 0)
+	
+
+
+func _on_input_prompt_end(success: bool) -> void:
+	minigame_end(success, 10 if success else 0)
--- minigames/hado_pizza/input_prompt.gd
+++ minigames/hado_pizza/input_prompt.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+class_name InputPrompt
+signal end(success: bool)
+
 enum Direction {
 	up = 1,
 	down = 2,
@@ -9,24 +12,23 @@ enum Direction {
 	OneDirection = 999
 }
 
-@export var directions: Array[Direction]
-@export var wrap_after: int = 20
+var directions: Array[Direction]
+var wrap_after: int = 10
 @onready var up_arrow: Sprite2D = $UpArrow
 @onready var xoffset: float = up_arrow.texture.get_size().x + 4 # 4 is a bit of margin
 @onready var yoffset: float = up_arrow.texture.get_size().y + 4 # 4 is a bit of margin
 
+var correct_guesses: int = 0
+
 func init(directions: Array[Direction]):
 	self.directions = directions
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
 	var x: float = xoffset / 2
 	var y: float = yoffset / 2
 	var index: int = 0
 	for dir in directions:
 		var current_dir = Sprite2D.new()
 		current_dir.texture = up_arrow.texture
-		
+		current_dir.name = "Direction" + str(index)
 		match dir:
 			Direction.down:
 				current_dir.transform = current_dir.transform.rotated(PI)
@@ -40,19 +42,48 @@ func _ready() -> void:
 		current_dir.position.y = y
 		x += xoffset
 		index += 1
-		if(index == wrap_after):
-			index = 0
+		if(index % wrap_after == 0):
 			x = xoffset / 2
 			y += yoffset
-		print(dir)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
 		
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("debug_key_1")):
-		var mychild: Sprite2D = get_child(2) # then sell it
+		var mychild: Sprite2D = get_child(1)
 		print(mychild.name)
 		var mytween = create_tween()
 		mytween.tween_property(mychild, "modulate:a", 0.6, 0.2).set_ease(Tween.EASE_IN_OUT)
-		
+	
+	var is_dir_pressed: bool = false
+	var correct_dir: bool = false
+	if(event.is_action_pressed("move_down")):
+		is_dir_pressed = true
+		if(directions[correct_guesses] == Direction.down):
+			correct_dir = true
+	elif(event.is_action_pressed("move_left")):
+		is_dir_pressed = true
+		if(directions[correct_guesses] == Direction.left):
+			correct_dir = true
+	elif(event.is_action_pressed("move_right")):
+		is_dir_pressed = true
+		if(directions[correct_guesses] == Direction.right):
+			correct_dir = true
+	elif(event.is_action_pressed("move_up")):
+		is_dir_pressed = true
+		if(directions[correct_guesses] == Direction.up):
+			correct_dir = true
+	
+	if(correct_dir):
+		var current_direction: Sprite2D = get_child(correct_guesses + 1)
+		var tween = create_tween()
+		tween.tween_property(current_direction, "modulate:a", 0.6, 0.2).set_ease(Tween.EASE_IN_OUT)
+		correct_guesses += 1
+		if(correct_guesses == len(directions)):
+			emit_signal("end", true)
+	elif(is_dir_pressed): emit_signal("end", false)
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
